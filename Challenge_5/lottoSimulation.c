006b33b5f00b247d0256ee57a45a6e0a6ed9c4f6
FelipeBaidon <felipe.baidon94@gmail.com>
2024-05-20 15:57:42 -0600

    Generated numbereded lotto balls

--- Challenge_5/lottoSimulation.c
+++ Challenge_5/lottoSimulation.c
@@ -1,4 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+#define NUMBER_OF_TICKETS 51
+#define WINNING_NUMBERS 6
+
 int main(void)
 {
+    /* numbers initialization */
+    int  i, tickets[NUMBER_OF_TICKETS];
+    
+
+    /* fill in participants in array */
+    for(i = 0; i < NUMBER_OF_TICKETS ; i++)
+    {
+        tickets[i] = i + 1; 
+    }
+
+   /* Print participants */
+   for(i = 0; i < NUMBER_OF_TICKETS ; i++)
+   {
+       printf("%d\n", tickets[i]);
+   
+   }
+   
     return 0;
 }
